86cbfc40e6fe5fd72c931cc3a4d6f4c9fad6f443
Anthony Damico <ajdamico@gmail.com>
2018-01-07 15:43:14 -0500

    ahs increase

--- tests/setup.R
+++ tests/setup.R
@@ -3,7 +3,7 @@ if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 library(lodown)
 lodown( "ahs" , output_dir = file.path( getwd() ) )
 this_sample_break <- Sys.getenv( "this_sample_break" )
-record_categories <- ceiling( seq( nrow( ahs_cat ) ) / ceiling( nrow( ahs_cat ) / 3 ) )
+record_categories <- ceiling( seq( nrow( ahs_cat ) ) / ceiling( nrow( ahs_cat ) / 5 ) )
 ahs_cat <- ahs_cat[ record_categories == this_sample_break , ]
 lodown( "ahs" , ahs_cat )
 if( any( ahs_cat$year == 2015 ) ){
