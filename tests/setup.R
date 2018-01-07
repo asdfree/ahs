3df0355a6cdb582f7071a295f07fbfc81e78579a
Anthony Damico <ajdamico@gmail.com>
2018-01-07 16:13:57 -0500

    chapter_tag_cat and fast_finish

--- tests/setup.R
+++ tests/setup.R
@@ -3,6 +3,7 @@ if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 library(lodown)
 lodown( "ahs" , output_dir = file.path( getwd() ) )
 this_sample_break <- Sys.getenv( "this_sample_break" )
+ahs_cat <- get_catalog( "ahs" , ahs_cat )
 record_categories <- ceiling( seq( nrow( ahs_cat ) ) / ceiling( nrow( ahs_cat ) / 5 ) )
 ahs_cat <- ahs_cat[ record_categories == this_sample_break , ]
 lodown( "ahs" , ahs_cat )
